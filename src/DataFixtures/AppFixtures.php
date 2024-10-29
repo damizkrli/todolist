8bda42a78f85f1006932fd1f1176a84dfd373ca8
Damien Carli <karli.damien@gmail.com>
2024-10-29 12:01:27 +0100

    fixtures user + installation faker

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 3; $i++) {
+            $user = new User();
+            $firstname = $faker->firstName();
+            $lastname = $faker->lastName();
+
+            $emailFirstname = strtolower(str_replace(['é', 'ê', 'è', 'ë', 'à', 'â', 'ç'], ['e','e','e','e','a','a','c'], $firstname));
+            $emailLastname = strtolower(str_replace(['é', 'ê', 'è', 'ë', 'à', 'â', 'ç'], ['e','e','e','e','a','a','c'], $lastname));
+            $domain = [
+                "gmail.com",
+                "hotmail.com",
+                "orange.fr",
+                "bbox.fr",
+                "free.fr",
+                "live.com",
+                "laposte.net",
+            ];
+
+            $user->setFirstname($firstname);
+            $user->setLastname($lastname);
+            $user->setEmail("{$emailFirstname}.{$emailLastname}@" . $domain[array_rand($domain)]);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
